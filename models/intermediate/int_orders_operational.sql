0b396fe790793840c867a765be69af18738c6167
munera.alfrawe@gmail.com <munera.alfrawe@gmail.com>
2024-03-05 22:15:04 +0000

    finance days commit

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,32 +1,16 @@
-WITH joined_data AS (
-    SELECT
-        o_margin.*,
-        ship.shipping_fee,
-        ship.logcost,
-        ship.ship_cost,
-        ship.orders_id
-    FROM
-        {{ ref('int_orders_margin') }} AS o_margin
-    INNER JOIN
-        {{ ref('stg_gz_raw_data__raw_gz_ship') }} AS ship
-    ON o_margin.orders_id = ship.orders_id
-    
-)
-SELECT
-    *,
-    ROUND(CAST(margin AS FLOAT64) + CAST(shipping_fee AS FLOAT64) - CAST(logcost AS FLOAT64) - CAST(ship_cost AS FLOAT64),2) AS operational_margin
-FROM
-    joined_data
-    
 
 SELECT
-  o_margin.*,
-  ,ROUND(o_margin.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
+  o.orders_id
+  ,o.date_date
+  ,ROUND(o.margin + s.shipping_fee - (s.logcost + CAST(s.ship_cost AS FLOAT64)),2) AS operational_margin 
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_cost
+  ,o.margin
   ,s.shipping_fee
   ,s.logcost
   ,s.ship_cost
-FROM {{ ref('int_orders_margin') }} o_margin
+FROM {{ ref('int_orders_margin') }} o
 LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_ship') }} s 
   USING(orders_id)
 ORDER BY orders_id desc
-    
